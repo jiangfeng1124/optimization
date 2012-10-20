5946aeb66752289d18f75026aca6dccdea8e7cfb
jiangfeng1124 <jiangfeng1124@gmail.com>
2012-10-20 16:38:16 -0400

    gradient descent using backtracking line search

--- src/gradient_descent.R
+++ src/gradient_descent.R
@@ -36,23 +36,23 @@ GradientDescent <- function(f, g, initial.x, tolerance = 10e-8, max.iterations =
 	return(optimization.result)
 }
 
-BacktrackingLineSearch <- function(f, g, x, dx, alpha = 0.1, beta = 0.8, max.iterations = 1000) {
+BacktrackingLineSearch <- function(f, g, x, dx, c = 0.1, rho = 0.8, max.iterations = 1000) {
 	i <- 0
 	
 	f.x <- f(x)
 	g.x <- g(x)
 	angle <- t(g.x) %*% dx
 	
-	t <- 1
-	while(f(x + t * dx) > f.x + alpha * t * angle) {
-		t <- beta * t
+	alpha <- 1
+	while(f(x + alpha * dx) > f.x + c * alpha * angle) {
+		alpha <- rho * alpha
 		i <- i + 1
 		
 		if(i > max.iterations) {
-			print(paste("too many iterations in BacktrackingLineSearch(alpha: ", alpha, " beta: ", beta))
+			print(paste("too many iterations in BacktrackingLineSearch(c: ", c, " rho: ", rho))
 		}
 	}
 
-	return(t)
+	return(alpha)
 }
 
